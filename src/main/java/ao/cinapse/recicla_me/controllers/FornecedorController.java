cc35e3312e502ad9de136da90ebcbf3d3f3f19ba
Ivandro de Sousa <48142347+ivanssousa@users.noreply.github.com>
2024-02-06 22:41:09 +0100

    refactor controllers

--- src/main/java/ao/cinapse/recicla_me/controllers/FornecedorController.java
+++ src/main/java/ao/cinapse/recicla_me/controllers/FornecedorController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/fornecedor")
+@RequestMapping("fornecedor")
 public class FornecedorController extends BaseController<ResponseBody, FornecedorDTO, UUID> {
 
     @Autowired
--- src/main/java/ao/cinapse/recicla_me/controllers/UsuarioController.java
+++ src/main/java/ao/cinapse/recicla_me/controllers/UsuarioController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ivandro.sousa
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("user")
 public class UsuarioController extends BaseController<ResponseBody, UsuarioDTO, UUID>  
 {
     @Autowired
