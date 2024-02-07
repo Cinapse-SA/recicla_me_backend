98769ad691f7b273ce2b0fddd531d8939f0f6857
Ivandro de Sousa <48142347+ivanssousa@users.noreply.github.com>
2024-02-08 00:35:26 +0100

    scan packages

--- src/main/java/ao/cinapse/recicla_me/ReciclaMeApplication.java
+++ src/main/java/ao/cinapse/recicla_me/ReciclaMeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ao.cinapse.recicla_me.controllers.auth","ao.cinapse.recicla_me.controllers"})
 public class ReciclaMeApplication {
 
 	public static void main(String[] args) {
