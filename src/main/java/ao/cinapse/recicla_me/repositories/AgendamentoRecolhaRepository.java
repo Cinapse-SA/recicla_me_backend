6de036c60a0938e50466d8d6797e765150e771ba
Ivandro de Sousa <ivandro2014sousa@gmail.com>
2024-10-21 23:44:40 +0100

    fix: feat -> confirmar recolha

--- src/main/java/ao/cinapse/recicla_me/repositories/AgendamentoRecolhaRepository.java
+++ src/main/java/ao/cinapse/recicla_me/repositories/AgendamentoRecolhaRepository.java
@@ -4,6 +4,7 @@ import ao.cinapse.recicla_me.models.AgendamentoRecolha;
 import ao.cinapse.recicla_me.models.EstadoAgendamentoRecolha;
 import ao.cinapse.recicla_me.models.Publicacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,8 +17,9 @@ public interface AgendamentoRecolhaRepository extends JpaRepository<AgendamentoR
 {
     List<AgendamentoRecolha> findByIdPublicacao( @Param("idPublicacao") Publicacao idPublicacao);
 
-    @Query("UPDATE agendamento_recolha agr SET agr.idEstadoAgendamentoRecolha = :cancelado WHERE agr.idPublicacao = :publicacao AND agr = :agendamento")
-    boolean confirmarAgendamentoRecolha(
+    @Modifying
+    @Query("UPDATE agendamento_recolha agr SET agr.idEstadoAgendamentoRecolha = :cancelado WHERE agr.idPublicacao = :publicacao AND agr != :agendamento")
+    Integer confirmarAgendamentoRecolha(
         @Param("publicacao") Publicacao publicacao,
         @Param("cancelado") EstadoAgendamentoRecolha cancelado,
         @Param("agendamento") AgendamentoRecolha agendamento
